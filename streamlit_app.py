68ecc76303de2dc8ecb729861d1070f2bfe1f3a6
maxomm <max.warsinke@googlemail.com>
2022-10-18 10:52:29 +0200

    tarifbereich

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,22 @@
 import streamlit as st
 import random
+import pandas as pd
+
+
+def translate_tarif(input):
+    return "A|B|C" if input == "ABC" else "A|B" if input == "AB" else "A"
+
 
 if __name__ == "__main__":
-    with open("station_list.txt", "r", encoding="utf8") as file:
-        stations = file.readlines()
-    st.title("Stations Test")
-    if st.button('Random Station'):
-        random_station = random.randrange(0,len(stations))
-        st.text(stations[random_station])
+    dfcsv = pd.read_csv("stations_newest.csv")
+    st.title("Stations")
+
+    option = st.selectbox("Tarifbereich", ("ABC", "AB", "A"))
+
+    if st.button("Random Station"):
+        filt = dfcsv[dfcsv["Tarifbereich"].str.contains(translate_tarif(option))]
+        random_station = random.randrange(0, len(filt))
+        st.text(filt["Name"].iloc[random_station])
+
+    st.subheader("All Stations")
+    st.dataframe(dfcsv, 1000, 200)
