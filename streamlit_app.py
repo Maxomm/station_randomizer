4f84b0da2ecdb4a1e3bd4db9a6989930ec9be140
maxomm <max.warsinke@googlemail.com>
2022-10-18 11:25:57 +0200

    trains

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,13 @@ if __name__ == "__main__":
     with col1:
         next_station_text = st.subheader(" ")
     with col2:
-        anbindung = st.subheader(" ")
+        trains = st.subheader(" ")
 
     if random_button:
         filt = dfcsv[dfcsv["Tarifbereich"].str.contains(translate_tarif(option))]
         random_station = random.randrange(0, len(filt))
         next_station_text.subheader(filt["Name"].iloc[random_station])
-        anbindung.subheader(filt["Anbindung"].iloc[random_station])
+        trains.subheader(filt["Anbindung"].iloc[random_station])
 
     # st.text("All Stations")
     st.empty()
