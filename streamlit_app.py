df143a64f6ca3cf5dfbdc5ca2d51fc1e526d9cf3
maxomm <max.warsinke@googlemail.com>
2022-10-18 11:25:10 +0200

    Anbindung

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,24 @@ def translate_tarif(input):
 
 if __name__ == "__main__":
     dfcsv = pd.read_csv("berlin_stations.csv")
-    st.title("Stations")
+    st.title("Berlin Stations")
 
     option = st.selectbox("Tarifbereich", ("ABC", "AB", "A"))
+    random_button = st.button("Random Station")
 
-    if st.button("Random Station"):
+    col1, col2 = st.columns(2)
+    with col1:
+        next_station_text = st.subheader(" ")
+    with col2:
+        anbindung = st.subheader(" ")
+
+    if random_button:
         filt = dfcsv[dfcsv["Tarifbereich"].str.contains(translate_tarif(option))]
         random_station = random.randrange(0, len(filt))
-        st.text(filt["Name"].iloc[random_station])
+        next_station_text.subheader(filt["Name"].iloc[random_station])
+        anbindung.subheader(filt["Anbindung"].iloc[random_station])
 
-    st.subheader("All Stations")
-    st.dataframe(dfcsv, 1000, 200)
+    # st.text("All Stations")
+    st.empty()
+    with st.expander("All Stations"):
+        st.dataframe(dfcsv, 1000, 1000)
