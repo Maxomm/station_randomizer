9d16690783839af6ac07ffc6087e048a9157cd78
maxomm <max.warsinke@googlemail.com>
2022-10-19 23:56:01 +0200

    added geolocation an map

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ def translate_tarif(input):
 if __name__ == "__main__":
     dfcsv = pd.read_csv("berlin_stations.csv")
     st.title("Berlin Stations")
-
     option = st.selectbox("Tarifbereich", ("ABC", "AB", "A"))
     random_button = st.button("Random Station")
 
@@ -20,11 +19,16 @@ if __name__ == "__main__":
     with col2:
         trains = st.subheader(" ")
 
+    filt = dfcsv[dfcsv["area"].str.contains(translate_tarif(option))]
+    mapdf = filt[["latitude", "longitude"]]
+    maps = st.map(mapdf)
+
     if random_button:
-        filt = dfcsv[dfcsv["Tarifbereich"].str.contains(translate_tarif(option))]
-        random_station = random.randrange(0, len(filt))
-        next_station_text.subheader(filt["Name"].iloc[random_station])
-        trains.subheader(filt["Anbindung"].iloc[random_station])
+        rnd_st = random.randrange(0, len(filt))
+        next_station_text.subheader(filt["name"].iloc[rnd_st])
+        trains.subheader(filt["trains"].iloc[rnd_st])
+        lat, lon = filt["latitude"].iloc[rnd_st], filt["longitude"].iloc[rnd_st]
+        maps.map(pd.DataFrame({"latitude": [lat], "longitude": [lon]}))
 
     with st.expander("All Stations"):
         st.dataframe(dfcsv, 1000, 1000)
