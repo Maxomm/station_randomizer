3afb7c922d8e611822ed70d2e6d4d96fcca6ad6c
maxomm <max.warsinke@googlemail.com>
2022-10-21 11:12:36 +0200

    cache data

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,27 +8,31 @@ def translate_tarif(opt):
     return "A|B|C" if opt == "ABC" else "A|B" if opt == "AB" else "A"
 
 
-dfcsv = pd.read_csv("berlin_stations.csv")
+@st.cache
+def load_data():
+    return pd.read_csv("berlin_stations.csv")
 
-if __name__ == "__main__":
-    st.title("Berlin Stations")
-    option = st.selectbox("Tarifbereich", ("ABC", "AB", "A"))
-    random_button = st.button("Random Station")
 
-    col1, col2 = st.columns(2)
-    with col1: next_station_text = st.subheader(" ")
-    with col2: trains = st.subheader(" ")
+dfcsv = load_data()
 
-    filt = dfcsv[dfcsv["area"].str.contains(translate_tarif(option))]
-    maps = st.map(filt[["latitude", "longitude"]])
+st.title("🚅 Berlin Stations")
+option = st.selectbox("Tarifbereich", ("ABC", "AB", "A"))
+random_button = st.button("Random Station")
 
-    if random_button:
-        rnd_st = random.randrange(0, len(filt))
-        next_station_text.subheader(filt["name"].iloc[rnd_st])
-        trains.subheader(filt["trains"].iloc[rnd_st])
-        lat = filt["latitude"].iloc[rnd_st]
-        lon = filt["longitude"].iloc[rnd_st]
-        maps.map(pd.DataFrame({"latitude": [lat], "longitude": [lon]}))
+filt = dfcsv[dfcsv["area"].str.contains(translate_tarif(option))]
+maps = st.map(filt[["latitude", "longitude"]])
 
-    with st.expander("All Stations"):
-        st.dataframe(dfcsv, 1000, 1000)
+col1, col2 = st.columns(2)
+with col1:
+    next_station_text = st.subheader("")
+with col2:
+    trains = st.subheader("")
+
+if random_button:
+    rnd_st = random.randrange(0, len(filt))
+    next_station_text.subheader(filt["name"].iloc[rnd_st])
+    trains.subheader(filt["trains"].iloc[rnd_st])
+
+    lat = filt["latitude"].iloc[rnd_st]
+    lon = filt["longitude"].iloc[rnd_st]
+    maps.map(pd.DataFrame({"latitude": [lat], "longitude": [lon]}))
